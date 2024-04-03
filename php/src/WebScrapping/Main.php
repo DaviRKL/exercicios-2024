57f6e6af220b6cd2c3b41a1f5a66fcc3cf0af496
DaviRKL <davirkl07@gmail.com>
2024-04-02 23:06:13 -0300

    Alterei a classe Main e a Scrapper para coletar e receber os dados, porem estou com problemas com os seletores

--- php/src/WebScrapping/Main.php
+++ php/src/WebScrapping/Main.php
@@ -2,20 +2,58 @@
 
 namespace Chuva\Php\WebScrapping;
 
+use DOMDocument;
+use Box\Spout\Writer\Common\Creator\WriterEntityFactory;
+
 /**
  * Runner for the Webscrapping exercice.
  */
-class Main {
+class Main
+{
 
   /**
    * Main runner, instantiates a Scrapper and runs.
    */
-  public static function run(): void {
+  public static function run(): void
+  {
+    libxml_use_internal_errors(true);
     $dom = new \DOMDocument('1.0', 'utf-8');
     $dom->loadHTMLFile(__DIR__ . '/../../assets/origin.html');
 
-    $data = (new Scrapper())->scrap($dom);
+    libxml_use_internal_errors(false);
 
+    $xpath = new \DOMXPath($dom);
+
+    $paperDiv = $xpath->query('//a[contains(@class, "paper-card p-lg bd-gradient-left")]');
+    $papers = [];
+
+    foreach ($paperDiv as $paper) {
+
+      $title = $xpath->query('.//h4[@class="my-xs paper-title"]', $paper)->item(0)->textContent;
+      $typeNode = $xpath->query('.//div[@class="tags mr-sm"]', $paper)->item(0);
+      $type = $typeNode ? $typeNode->textContent : '';
+
+      $authorNodes = $xpath->query('.//span[@class="authors"]//span', $paper);
+      $authors = [];
+      foreach ($authorNodes as $authorNode) {
+        $authorName = $authorNode->textContent;
+        $authorInstitution = $authorNode->getAttribute('title');
+        $authors[] = new Entity\Person($authorName, $authorInstitution);
+      }
+      $papers[] = new Entity\Paper($title, $type, $authors);
+    }
+    $scrapper = new Scrapper();
+
+    $data = $scrapper->scrap(
+      $dom,
+      'paper-card p-lg bd-gradient-left',
+      'a',
+      'h4.my-xs.paper-title',
+      'span.authors span',
+      'div.tags.mr-sm',
+      'span.name',
+      'div.tags.flex-row.mr-sm > div.volume-info'
+    );
     // Write your logic to save the output file bellow.
     print_r($data);
   }
--- php/src/WebScrapping/Scrapper.php
+++ php/src/WebScrapping/Scrapper.php
@@ -13,18 +13,39 @@ class Scrapper {
   /**
    * Loads paper information from the HTML and returns the array with the data.
    */
-  public function scrap(\DOMDocument $dom): array {
-    return [
-      new Paper(
-        123,
-        'The Nobel Prize in Physiology or Medicine 2023',
-        'Nobel Prize',
-        [
-          new Person('Katalin Karikó', 'Szeged University'),
-          new Person('Drew Weissman', 'University of Pennsylvania'),
-        ]
-      ),
-    ];
+  public function scrap(\DOMDocument $dom , string $className, string $elementTag, string $titleTag, string $typeTag, string $authorContainerTag, string $authorNameTag, string $authorInstitutionTag): array {
+    $papers = [];
+
+    $xpath = new \DOMXPath($dom);
+    $elements = $xpath->query("//{$elementTag}[contains(@class, '{$className}')]");
+
+    foreach($elements as $element) {
+      $title = $xpath->query(".//{$titleTag}", $element)->item(0)->textContent;
+      $type = $xpath->query(".//{$typeTag}", $element)->item(0)->textContent;
+
+      $authors = [];
+      $authorContainers = $xpath->query(".//{$authorContainerTag}", $element);
+
+      foreach ($authorContainers as $authorContainer) {
+        $authorName = $xpath->query(".//{$authorNameTag}", $authorContainer)->item(0)->textContent;
+        $authorInstitution = $xpath->query(".//{$authorInstitutionTag}", $authorContainer)->item(0)->textContent;
+
+        $authors[] = new Person($authorName, $authorInstitution);
+    }
+    $papers[] = new Paper($title, $type, $authors);
+    }
+    return $papers; 
+    // return [
+    //   new Paper(
+    //     123,
+    //     'The Nobel Prize in Physiology or Medicine 2023',
+    //     'Nobel Prize',
+    //     [
+    //       new Person('Katalin Karikó', 'Szeged University'),
+    //       new Person('Drew Weissman', 'University of Pennsylvania'),
+    //     ]
+    //   ),
+    // ];
   }
 
 }
