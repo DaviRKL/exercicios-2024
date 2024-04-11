d39959816b44fd904b8080515468904f93c8df62
DaviRKL <davirkl07@gmail.com>
2024-04-11 08:01:37 -0300

    Corrige a classe HTMLUtils

--- php/src/WebScrapping/Util/HTMLUtils.php
+++ php/src/WebScrapping/Util/HTMLUtils.php
@@ -7,13 +7,19 @@ namespace Chuva\Php\WebScrapping\Util;
  */
 class HTMLUtils
 {
+
     /**
      * Finds elements by attribute and value.
      *
-     * @param  string $attribute The attribute to search for.
-     * @param  string $value     The value of the attribute.
-     * @param  array  $tagList   The list of HTML tags to search in.
-     * @return array The elements found.
+     * @param string $attribute
+     *   The attribute to search for.
+     * @param string $value
+     *   The value of the attribute.
+     * @param array  $tagList
+     *   The list of HTML tags to search in.
+     *
+     * @return array
+     *   The elements found.
      */
     public static function findElementsByAttributeAndValue($attribute, $value, $tagList)
     {
@@ -27,9 +33,10 @@ class HTMLUtils
                 if (in_array($value, $arrayClass)) {
                     $elementsFound[] = $tag;
                 }
+
             } elseif ($tagAttributeValue === $value) {
                 $elementsFound[] = $tag;
-            }
+            }  
         }
 
         return $elementsFound;
@@ -38,10 +45,15 @@ class HTMLUtils
     /**
      * Finds an element by attribute and value.
      *
-     * @param  string $attribute The attribute to search for.
-     * @param  string $value     The value of the attribute.
-     * @param  array  $tagList   The list of HTML tags to search in.
-     * @return mixed|null The element found, or null if not found.
+     * @param string $attribute
+     *   The attribute to search for.
+     * @param string $value
+     *   The value of the attribute.
+     * @param array  $tagList
+     *   The list of HTML tags to search in.
+     *
+     * @return mixed|null
+     *   The element found, or null if not found.
      */
     public static function findElementByAttributeAndValue($attribute, $value, $tagList)
     {
@@ -53,6 +65,7 @@ class HTMLUtils
                 if (in_array($value, $arrayClass)) {
                     return $tag;
                 }
+
             } elseif ($tagAttributeValue === $value) {
                 return $tag;
             }
@@ -64,9 +77,13 @@ class HTMLUtils
     /**
      * Finds an element by class.
      *
-     * @param  \DOMDocument $dom       The DOM document to search in.
-     * @param  string       $className The class name to search for.
-     * @return \DOMNodeList The elements found.
+     * @param \DOMDocument $dom
+     *   The DOM document to search in.
+     * @param string       $className
+     *   The class name to search for.
+     *
+     * @return \DOMNodeList
+     *   The elements found.
      */
     public static function findElementByClass($dom, $className)
     {
@@ -74,4 +91,5 @@ class HTMLUtils
         $elementsFound = $xPath->query("//*[@class='$className']");
         return $elementsFound;
     }
+
 }
